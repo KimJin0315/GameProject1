bdcfae5016fee1b85845ba41e1e6d1380d4ac8b6
KimJin0315 <xnxl159@naver.com>
2025-04-21 19:44:08 +0900

    (2025.04.21) 이벤트 트리거 시스템

--- Game/Assets/Class7th (User Interface)/Scripts/ButtonManager.cs
+++ Game/Assets/Class7th (User Interface)/Scripts/ButtonManager.cs
@@ -1,10 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ButtonManager : MonoBehaviour
 {
-   
+    [SerializeField] Text[] textlist;
+    [SerializeField] string[] titlelist;
+
+    private void Start()
+    {
+        for (int i = 0; i < textlist.Length; i++)
+        {
+            textlist[i].text = titlelist[i];
+        }
+    }
+
     public void Execute()
     {
         Debug.Log("Execute");
@@ -13,12 +24,10 @@ public class ButtonManager : MonoBehaviour
     public void Shop()
     {
         Debug.Log("Shop");
-
     }
 
     public void Quit()
     {
         Debug.Log("Quit");
-
     }
 }
