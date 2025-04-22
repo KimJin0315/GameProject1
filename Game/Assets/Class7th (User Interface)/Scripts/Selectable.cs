cb17b5e842efa65edc2e31cd40e5ecb371da6133
KimJin0315 <xnxl159@naver.com>
2025-04-22 19:44:25 +0900

    (2025.04.22) 이동 시스템

--- Game/Assets/Class7th (User Interface)/Scripts/Selectable.cs
+++ Game/Assets/Class7th (User Interface)/Scripts/Selectable.cs
@@ -10,16 +10,19 @@ public class Selectable : MonoBehaviour
     public void Entered()
     {
         titleText.fontSize = 100;
+        titleText.color = Color.green;
     }
 
     public void Exited()
     {
         titleText.fontSize = 75;
+        titleText.color = Color.white;
     }
 
     public void Selected()
     {
         titleText.fontSize = 50;
+        titleText.color = Color.green;
     }
 
 
