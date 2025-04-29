0e725f2330404339bd60fb632d59d2549a87a457
KimJin0315 <xnxl159@naver.com>
2025-04-29 19:44:50 +0900

    (2025.04.29) 물리 시스템

---
+++ Game/Assets/Clas10th (Instantiate & Destroy)/Scripts/Weapon.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Weapon : MonoBehaviour
+{
+
+    [SerializeField] protected int damage;
+    [SerializeField] protected float intersection;
+    abstract public void Attack();
+      
+
+}
--- Game/Assets/Clas10th (Instantiate & Destroy)/Scripts/WeaponManager.cs
+++ Game/Assets/Clas10th (Instantiate & Destroy)/Scripts/WeaponManager.cs
@@ -2,19 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class WeaponManager : MonoBehaviour
 {
-    [SerializeField] GameObject prefab;
+    [SerializeField] GameObject [] weapons;
+    [SerializeField] List<GameObject> weaponList;
+
     [SerializeField] Transform parentPostion;
 
-    [SerializeField] WeaponManager clone;
+
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(prefab, parentPostion);
+        for (int i = 0; i < weapons.Length; i++)
+        {
+            Instantiate(weapons[i], parentPostion);
+        }
 
-        
-        Debug.Log(" ");
     }
+
+    void Update()
+    {
+        if(//스페이스바 눌렸을 때)
+        {
+            for (int i = 0; i < weapons.Length; i++)
+            {
+                weaponList.Add(weapons[i]);
+
+                GameObject.SetActive(weapons[i - 1]) = false;
+            }
+        }
+        
+    }
+
 }
